a983b086644b131284fd3744c2238f5bffeb3902
Abhishek Jain <abhishekjain5820@gmail.com>
2023-10-27 13:13:51 +0530

    first commit

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Abhishek here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Palolem Beach Goa India</h1>
+          <p className={styles.postDesc}>
+            I visited this place in the month of july.It was the best time to visit this place
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
